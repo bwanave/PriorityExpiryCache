d7b923189cc2d09ab59de3563218274e2d668d03
Balaji Wanave <balajiwanave1990@gmail.com>
2022-08-30 13:23:08 +0530

    #1: Priority Expiry Cache implementation

--- src/main/java/com/example/list/DoublyLinkedList.java
+++ src/main/java/com/example/list/DoublyLinkedList.java
@@ -16,11 +16,9 @@ public class DoublyLinkedList<T> {
     }
 
     public void remove(Node<T> node) {
-        if (node == head) this.head = this.tail = null;
-        else if (node == tail) {
-            tail = tail.prev;
-            tail.next = null;
-        }
+        if (node == head && node == tail) this.head = this.tail = null;
+        else if (node == head) head = head.next;
+        else if (node == tail) tail = tail.prev;
         else {
             node.prev.next = node.next;
             node.next.prev = node.prev;
